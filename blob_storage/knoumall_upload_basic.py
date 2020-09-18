6e024dd828f3ac2658d0b748d0dc6e05a8dfb7ed
jiho lee <search5@gmail.com>
2020-09-18 17:12:47 +0900

    unessory code delete

--- blob_storage/knoumall_upload_basic.py
+++
@@ -1,9 +0,0 @@
-import os, uuid
-from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
-
-try:
-    print("한국방송통신대학교 4학년 2학기 클라우드 컴퓨팅")
-    # 여기에 코드를 작성한다
-except Exception as ex:
-    print('Exception:')
-    print(ex)
