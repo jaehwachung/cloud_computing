8ef4a55a4f715f7ad967454271f39294861571df
jaehwachung <jaehwachung@knou.ac.kr>
2023-03-04 17:52:48 +0900

    Update table.sql

--- shop2/table.sql
+++ shop2/table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE shop_member (
-	id INTEGER NOT NULL IDENTITY(1,1), 
+	id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY, 
 	name VARCHAR(50) NULL, 
 	email VARCHAR(200) NULL, 
 	password VARCHAR(32) NULL, 
@@ -7,13 +7,13 @@ CREATE TABLE shop_member (
 	address VARCHAR(255) NULL, 
 	detail_address VARCHAR(255) NULL, 
 	is_admin CHAR(1) NULL, 
-	create_date DATETIME NULL, 
+	create_date TIMESTAMP NULL, 
 	PRIMARY KEY (id), 
 	UNIQUE (email)
 )
 
 CREATE TABLE goods (
-	id INTEGER NOT NULL IDENTITY(1,1), 
+	id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY, 
 	goods_name VARCHAR(255) NULL, 
 	price INTEGER NULL, 
 	goods_photo VARCHAR(255) NULL, 
@@ -24,16 +24,16 @@ CREATE TABLE goods (
 )
 
 CREATE TABLE orders (
-	id INTEGER NOT NULL IDENTITY(1,1), 
+	id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY, 
 	order_str_id VARCHAR(100) NULL, 
 	member INTEGER NULL, 
-	order_date DATETIME NULL, 
+	order_date TIMESTAMP NULL, 
 	PRIMARY KEY (id), 
 	FOREIGN KEY(member) REFERENCES shop_member (id)
 )
 
 CREATE TABLE orders_item (
-	id INTEGER NOT NULL IDENTITY(1,1), 
+	id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY, 
 	goods INTEGER NULL, 
 	goods_price INTEGER NULL, 
 	goods_cnt INTEGER NULL, 
@@ -42,7 +42,7 @@ CREATE TABLE orders_item (
 )
 
 CREATE TABLE basket (
-	id INTEGER NOT NULL IDENTITY(1,1), 
+	id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY, 
 	member INTEGER NULL, 
 	goods INTEGER NULL, 
 	goods_cnt INTEGER NULL, 
@@ -52,10 +52,10 @@ CREATE TABLE basket (
 )
 
 CREATE TABLE goods_tracking (
-	id INTEGER NOT NULL IDENTITY(1,1), 
+	id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY, 
 	order_id INTEGER NULL, 
-	delivery_start_date DATETIME NULL, 
-	delivery_end_date DATETIME NULL, 
+	delivery_start_date TIMESTAMP NULL, 
+	delivery_end_date TIMESTAMP NULL, 
 	tracking_number VARCHAR(50) NULL, 
 	tracking_status VARCHAR(30) NULL, 
 	PRIMARY KEY (id), 
