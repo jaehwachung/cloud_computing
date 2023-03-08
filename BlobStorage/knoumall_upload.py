260c5813d4e629ca8366b198dc44ff48c43bdd96
Jiho Persy Lee <search5@gmail.com>
2023-03-09 03:54:44 +0900

    Update knoumall_upload.py
    오탈자 수정

--- BlobStorage/knoumall_upload.py
+++ BlobStorage/knoumall_upload.py
@@ -4,7 +4,7 @@ from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 try:
   print("한국방송통신대학교 4학년 2학기 클라우드 컴퓨팅")
   connect_str = "<연결 문자열>"
-  blob_service_client = BlobServiceClient.from_connection_ string(connect_str)
+  blob_service_client = BlobServiceClient.from_connection_string(connect_str)
   container_name = "cloud-shop-cc"
 
   local_file_name = "quickstart" + str(uuid.uuid4()) + ".txt" 
@@ -12,9 +12,9 @@ try:
   
   file = open(upload_file_path,'w') 
   file.write("Hello, World!") 
-  file.close( )
+  file.close()
 
-  blob_client = blob_service_client.get_blob_client(container=container_ name, blob=local_file_name)
+  blob_client = blob_service_client.get_blob_client(container=container_name, blob=local_file_name)
   
   print("\nUploading to Azure Storage as blob:\n\t" + local_file_name)
 
