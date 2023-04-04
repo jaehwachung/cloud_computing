63589e650cea3b2c0ad86e0ae72fcc914c4725e7
jaehwachung <jaehwachung@knou.ac.kr>
2023-04-04 11:40:58 +0900

    Update knoumall_download.py

--- BlobStorage/knoumall_download.py
+++ BlobStorage/knoumall_download.py
@@ -7,14 +7,14 @@ try:
   connect_str = "<연결 문자열>"
   blob_service_client = BlobServiceClient.from_connection_string(connect_str)
   container_name = "mall-blob-container"
-  local_file_name = "quickstart" + str(uuid.uuid4()) + ".txt" 
+  remote_file_name = "quickstart" + str(uuid.uuid4()) + ".txt" 
   download_file_path = local_file_name
-  file = open(upload_file_path,'w')
+  file = open(download_file_path,'w')
   file.write("Hello, World!")
   file.close()
 
-  blob_client = blob_service_client.get_blob_client(container=container_name, blob=local_file_name)
-  print("\nDownloading from Azure Storage to local:\n\t" + local_file_name)
+  blob_client = blob_service_client.get_blob_client(container=container_name, blob=remote_file_name)
+  print("\nDownloading from Azure Storage to local:\n\t" + remote_file_name)
 
   # Upload the created file
   with open(download_file_path, "rb") as data:
