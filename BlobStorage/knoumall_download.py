4bd36710d91dc4c32b281e88610a0c66a397aef7
jaehwachung <jaehwachung@knou.ac.kr>
2023-04-04 10:23:39 +0900

    Update knoumall_download.py

--- BlobStorage/knoumall_download.py
+++ BlobStorage/knoumall_download.py
@@ -14,11 +14,11 @@ try:
   file.close()
 
   blob_client = blob_service_client.get_blob_client(container=container_name, blob=local_file_name)
-  print("\nUploading to Azure Storage as blob:\n\t" + local_file_name)
+  print("\nDownloading from Azure Storage to local:\n\t" + local_file_name)
 
   # Upload the created file
-  with open(upload_file_path, "rb") as data:
-    blob_client.upload_blob(data)
+  with open(download_file_path, "rb") as data:
+    blob_client.download_blob(data)
 
 except Exception as ex: 
   print('Exception:')
