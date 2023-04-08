0853849f522f0cab25f70dfb998730f77dba4398
jaehwachung <jaehwachung@knou.ac.kr>
2023-04-08 10:31:17 +0900

    Update knoumall_download.py

--- blob_storage/knoumall_download.py
+++ blob_storage/knoumall_download.py
@@ -1,26 +1,21 @@
-import os, uuid
-from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
+import os
+from azure.storage.blob import BlobServiceClient
 
 try:
-  print("한국방송통신대학교 4학년 2학기 클라우드 컴퓨팅")
-
+  print("한국방송통신대학교 클라우드 컴퓨팅 Blob 파일 다운로드")
   connect_str = "<연결 문자열>"
   blob_service_client = BlobServiceClient.from_connection_string(connect_str)
-  container_name = "mall-blob-container"
-  remote_file_name = "quickstart" + str(uuid.uuid4()) + ".txt" 
-  download_file_path = local_file_name
-  file = open(download_file_path,'w')
-  file.write("Hello, World!")
-  file.close()
+  container_name = "<컨테이너 이름>"
+
+  remote_file_name = "main_carousel.png" 
+  local_file_name = "main_carousel_down.png"
 
   blob_client = blob_service_client.get_blob_client(container=container_name, blob=remote_file_name)
-  print("\nDownloading from Azure Storage to local:\n\t" + remote_file_name)
 
-  # Upload the created file
-  with open(download_file_path, "rb") as data:
-    blob_client.download_blob(data)
+  with open(file=os.path.join('', local_file_name), mode="wb") as download_blob:
+        download_stream = blob_client.download_blob()
+        download_blob.write(download_stream.readall())
 
 except Exception as ex: 
   print('Exception:')
   print(ex)
-  
