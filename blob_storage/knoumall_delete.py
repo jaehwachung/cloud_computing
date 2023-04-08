2b31576c7782776fc2de9d441a5cb1a181508fa8
jaehwachung <jaehwachung@knou.ac.kr>
2023-04-08 10:46:51 +0900

    Update knoumall_delete.py

--- blob_storage/knoumall_delete.py
+++ blob_storage/knoumall_delete.py
@@ -5,13 +5,13 @@ try:
   print("한국방송통신대학교 4학년 2학기 클라우드 컴퓨팅")
   connect_str = "<연결 문자열>"
   blob_service_client = BlobServiceClient.from_connection_string(connect_str)
-  container_name = "cloud-shop-cc"
+  container_name = "<컨테이터 이름>"
 
-  local_file_name = "quickstart-sample.txt"
+  remote_file_name = "main_carousel.png"
 
-  blob_client = blob_service_client.get_blob_client(container=container_name, blob=local_file_name)
+  blob_client = blob_service_client.get_blob_client(container=container_name, blob=remote_file_name)
 
-  print("\nDelete blob to \n\t" + local_file_name)
+  print("\nDelete blob \n\t" + remote_file_name)
   blob_client.delete_blob()
 
   except Exception as ex: 
