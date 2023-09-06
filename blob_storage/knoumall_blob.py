24b4984d497e15ee1b40a3580a9e8e2a80668699
jaehwachung <jaehwachung@knou.ac.kr>
2023-09-06 14:35:17 +0900

    Create knoumall_blob.py

---
+++ blob_storage/knoumall_blob.py
@@ -0,0 +1,10 @@
+import os
+from azure.storage.blob import BlobServiceClient
+
+try :
+    print("한국방송통신대학교 4학년 2학기 클라우드 컴퓨팅")
+    # 여기에 코드를 작성한다.
+
+except Exception as ex:
+    print('Exception:')
+    print(ex)
